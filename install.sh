5cb5f05642b4b4b832c64e2a4b1a03092469e79f
Darius Sas <darius.sas@outlook.com>
2025-01-31 09:10:43 +0100

    Updated post install instructions

--- install.sh
+++ install.sh
@@ -244,4 +244,4 @@ runtime_generation
 echo "Installation completed!"
 echo "To start the application, run: ./run-impact.sh"
 echo "The dashboard should be accessible at ${PROTOCOL}://${IP}:3000"
-echo "To analyze local repositories set, in the dashboard, the local path to /repo/<folder name>"
+echo "To analyze local repositories set, in the dashboard, the local path to /repos/<folder name>"
