1a82396d672add12e8ddbbb217ec317b98dc4171
Darius Sas <darius.sas@outlook.com>
2025-03-13 15:41:06 +0100

    mac os support

--- run-impact.sh
+++ run-impact.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-MY_UID="$(id -u)" MY_GID="$(id -g)" docker --config ./docker-conf compose up $@
+
+# Detect the OS
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Run macOS-specific command
+    EFFECTIVE_USER=$(id -un)
+    DOCKER_HOST=unix:///Users/$EFFECTIVE_USER/.docker/run/docker.sock MY_UID="$(id -u)" MY_GID="$(id -g)" docker --config ./docker-conf compose up $@
+else
+    MY_UID="$(id -u)" MY_GID="$(id -g)" docker --config ./docker-conf compose up $@
+fi
